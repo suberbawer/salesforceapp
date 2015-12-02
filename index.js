ce0a2cde3e404261ae26456328251db1be661d1f
santi <santiago@oktana.io>
2015-12-02 20:44:31 -0300

    s

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var sf = require('node-salesforce');
 var express = require('express');
 var app = express();
 
+app.use(bodyParser.json());
+
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
