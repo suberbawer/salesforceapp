23f30a24cac9f966c910ec6ea367575571485724
santi <santiago@oktana.io>
2015-12-02 14:52:42 -0300

    s

--- index.js
+++ index.js
@@ -18,17 +18,15 @@ app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
-
 var oauth2 = new sf.OAuth2({
-  // you can change loginUrl to connect to sandbox or prerelease env.
+  // we can change loginUrl to connect to sandbox or prerelease env.
   // loginUrl : 'https://test.salesforce.com',
-  clientId : '3MVG9KI2HHAq33RwhxMVCRBe6dPIkfPSR1aQ1yyIPMtuCSZnKT5_XBo.lHvzHDuqQq4RhGKIqAhR2aYM2yCO2',
-  clientSecret : '3102684591552334404',
+  clientId : '3MVG91ftikjGaMd_epnylI.6EF7HD13f4Vz5k27V.mtepNErOxzFVdczAIGPkckY57Uy5V9EK5UohtiJM00t7',
+  clientSecret : '4671395917099215169',
   redirectUri : 'https://salesforceapi.herokuapp.com'
 });
-//
+
 // Get authz url and redirect to it.
-//
 app.get('/oauth2/auth', function(req, res) {
   res.redirect(oauth2.getAuthorizationUrl({ scope : 'api id web' }));
 });
