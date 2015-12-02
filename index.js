ba828b0e3c9460f7a4a5964026812a8a1cee7714
santi <santiago@oktana.io>
2015-12-02 20:30:36 -0300

    s

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
-app.use(express.bodyParser());
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
