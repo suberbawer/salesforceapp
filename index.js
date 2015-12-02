080014157060eca5a79232f6c5ce00558e9ff7ce
santi <santiago@oktana.io>
2015-12-02 18:28:56 -0300

    s

--- index.js
+++ index.js
@@ -81,7 +81,8 @@ express.Router().post('/', function(req, res) {
 //     });
 // });
 app.get('/test', function(req, res) {
-}
+    return 'hola';
+});
 app.post('/', function(req, res) {
     console.log('------- ', req.body.content);
     res.send('SUCESS');
