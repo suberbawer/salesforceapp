e9b1da6824ee51f392419e2b593b6f8b761d1779
santi <santiago@oktana.io>
2015-12-02 16:23:59 -0300

    s

--- index.js
+++ index.js
@@ -31,6 +31,22 @@ app.get('/oauth2/auth', function(req, res) {
   res.redirect(oauth2.getAuthorizationUrl({ scope : 'api id web' }));
 });
 
+app.get('/oauth2/callback', function(req, res) {
+  var conn = new sf.Connection({ oauth2 : oauth2 });
+  var code = req.param('code');
+  conn.authorize(code, function(err, userInfo) {
+    if (err) { return console.error(err); }
+    // Now you can get the access token, refresh token, and instance URL information.
+    // Save them to establish connection next time.
+    console.log(conn.accessToken);
+    console.log(conn.refreshToken);
+    console.log(conn.instanceUrl);
+    console.log("User ID: " + userInfo.id);
+    console.log("Org ID: " + userInfo.organizationId);
+    // ...
+  });
+});
+
 app.post('/', function(req, res) {
     console.log('------- ', req.body.content);
     res.send('SUCESS');
