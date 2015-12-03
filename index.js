f79c282f1c1c75637777601cb9ff38c0423476f4
santi <santiago@oktana.io>
2015-12-03 10:40:41 -0300

    s

--- index.js
+++ index.js
@@ -16,29 +16,25 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-    console.log('asdfasdfasdfasdfasdfasdf');
-  response.render('pages/index');
+var oauth2 = new sf.OAuth2({
+    // we can change loginUrl to connect to sandbox or prerelease env.
+    // loginUrl : 'https://test.salesforce.com',
+    clientId : '3MVG91ftikjGaMd_epnylI.6EF7HD13f4Vz5k27V.mtepNErOxzFVdczAIGPkckY57Uy5V9EK5UohtiJM00t7',
+    clientSecret : '4671395917099215169',
+    redirectUri : 'https://salesforceapi.herokuapp.com'
+});
+
+// Get authz url and redirect to it.
+app.get('/', function(req, res) {
+    console.log('---------------------- estoy adentro de la autorizacion');
+    res.redirect(oauth2.getAuthorizationUrl({ scope : 'api id web' }));
 });
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
-var oauth2 = new sf.OAuth2({
-  // we can change loginUrl to connect to sandbox or prerelease env.
-  // loginUrl : 'https://test.salesforce.com',
-  clientId : '3MVG91ftikjGaMd_epnylI.6EF7HD13f4Vz5k27V.mtepNErOxzFVdczAIGPkckY57Uy5V9EK5UohtiJM00t7',
-  clientSecret : '4671395917099215169',
-  redirectUri : 'https://salesforceapi.herokuapp.com'
-});
-
-// // Get authz url and redirect to it.
-app.get('/oauth2/auth', function(req, res) {
-    res.redirect(oauth2.getAuthorizationUrl({ scope : 'api id web' }));
-});
-
-app.get('/oauth2/callback', function(req, res) {
+app.get('/callback', function(req, res) {
     var conn = new sf.Connection({ oauth2 : oauth2 });
     var code = req.param('code');
     conn.authorize(code, function(err, userInfo) {
@@ -66,9 +62,25 @@ app.get('/oauth2/callback', function(req, res) {
     });
 });
 
+// var records = [];
+// conn.query("SELECT Id, Name FROM Account", function(err, result) {
+//     if (!err) {
+//         console.log("total : " + result.totalSize);
+//         console.log("fetched : " + result.records.length);
+//     }
+// });
+
 app.post('/test', function(req, res) {
-    //var hola = req.body.content;
+    var message = 'ERROR';
     attIds = req.body;
     console.log('5234523452345243524352345234523452345234 ', attIds);
-    res.send(attIds);
+    console.log('-----------------------------  ', res);
+    console.log('-----------------------------  ', res.success);
+    if (attId) {
+        message = 'SUCCESS';
+        if (res.success) {
+            res.redirect('/');
+        }
+    }
+    res.send(message);
 });
