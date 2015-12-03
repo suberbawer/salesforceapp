abd81d8bae3790eb15a0d3078dbe5a83167bbba0
santi <santiago@oktana.io>
2015-12-03 16:57:54 -0300

    s

--- index.js
+++ index.js
@@ -38,30 +38,30 @@ app.get('/', function(req, res) {
     var conn = new sf.Connection({ oauth2 : oauth2 });
     let code = req.query.code;
     console.log('-------------code', req.query.code);
-    conn.authorize('aPrxlJVwTHScBoWw2sd2B4ydT8qioIzre5e6.JEnSBC5pg0oyoO0dOI3.18URSVMdVGERmIhtw%3D%3D', function(err, userInfo) {
+    conn.authorize(code, function(err, userInfo) {
         if (err) { return console.log('erroooooooooor ',err); }
         // Now you can get the access token, refresh token, and instance URL information.
         // Save them to establish connection next time.
-        // console.log('-----token', conn.accessToken);
-        // console.log('------refresh', conn.refreshToken);
-        // console.log('------url ', conn.instanceUrl);
-        // console.log('***********userInfo ', userInfo);
-        // console.log("User ID: " + userInfo.id);
-        // console.log("Org ID: " + userInfo.organizationId);
+        console.log('-----token', conn.accessToken);
+        console.log('------refresh', conn.refreshToken);
+        console.log('------url ', conn.instanceUrl);
+        console.log('***********userInfo ', userInfo);
+        console.log("User ID: " + userInfo.id);
+        console.log("Org ID: " + userInfo.organizationId);
         // ...
     });
-    var records = [];
-    conn.query("SELECT Id, Name FROM Account", function(err, result) {
-        if (err) { return console.error(err); }
-        console.log("total : " + result.totalSize);
-        console.log("fetched : " + result.records.length);
-        console.log("done ? : " + result.done);
-        if (!result.done) {
-            // you can use the locator to fetch next records set.
-            // Connection#queryMore()
-            console.log("next records URL : " + result.nextRecordsUrl);
-        }
-    });
+    // var records = [];
+    // conn.query("SELECT Id, Name FROM Account", function(err, result) {
+    //     if (err) { return console.error(err); }
+    //     console.log("total : " + result.totalSize);
+    //     console.log("fetched : " + result.records.length);
+    //     console.log("done ? : " + result.done);
+    //     if (!result.done) {
+    //         // you can use the locator to fetch next records set.
+    //         // Connection#queryMore()
+    //         console.log("next records URL : " + result.nextRecordsUrl);
+    //     }
+    // });
 });
 
 // var records = [];
