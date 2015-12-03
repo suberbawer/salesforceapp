75e9c46c2bebc44307853eb1cc4f99c9e18a5360
santi <santiago@oktana.io>
2015-12-02 21:12:37 -0300

    s

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var sf = require('node-salesforce');
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var attIds;
 
 app.use(bodyParser());
 app.use(bodyParser.json());
@@ -36,37 +37,38 @@ var oauth2 = new sf.OAuth2({
 app.get('/oauth2/auth', function(req, res) {
     res.redirect(oauth2.getAuthorizationUrl({ scope : 'api id web' }));
 });
-//
-// app.get('/oauth2/callback', function(req, res) {
-//     var conn = new sf.Connection({ oauth2 : oauth2 });
-//     var code = req.param('code');
-//     conn.authorize(code, function(err, userInfo) {
-//         if (err) { return console.error(err); }
-//         // Now you can get the access token, refresh token, and instance URL information.
-//         // Save them to establish connection next time.
-//         console.log(conn.accessToken);
-//         console.log(conn.refreshToken);
-//         console.log(conn.instanceUrl);
-//         console.log("User ID: " + userInfo.id);
-//         console.log("Org ID: " + userInfo.organizationId);
-//         // ...
-//     });
-//     var records = [];
-//     conn.query("SELECT Id, Name FROM Account", function(err, result) {
-//         if (err) { return console.error(err); }
-//         console.log("total : " + result.totalSize);
-//         console.log("fetched : " + result.records.length);
-//         console.log("done ? : " + result.done);
-//         if (!result.done) {
-//             // you can use the locator to fetch next records set.
-//             // Connection#queryMore()
-//             console.log("next records URL : " + result.nextRecordsUrl);
-//         }
-//     });
-// });
+
+app.get('/oauth2/callback', function(req, res) {
+    var conn = new sf.Connection({ oauth2 : oauth2 });
+    var code = req.param('code');
+    conn.authorize(code, function(err, userInfo) {
+        if (err) { return console.error(err); }
+        // Now you can get the access token, refresh token, and instance URL information.
+        // Save them to establish connection next time.
+        console.log(conn.accessToken);
+        console.log(conn.refreshToken);
+        console.log(conn.instanceUrl);
+        console.log("User ID: " + userInfo.id);
+        console.log("Org ID: " + userInfo.organizationId);
+        // ...
+    });
+    var records = [];
+    conn.query("SELECT Id, Name FROM Account", function(err, result) {
+        if (err) { return console.error(err); }
+        console.log("total : " + result.totalSize);
+        console.log("fetched : " + result.records.length);
+        console.log("done ? : " + result.done);
+        if (!result.done) {
+            // you can use the locator to fetch next records set.
+            // Connection#queryMore()
+            console.log("next records URL : " + result.nextRecordsUrl);
+        }
+    });
+});
+
 app.post('/test', function(req, res) {
     //var hola = req.body.content;
-    var hola = req.body;
-    console.log('5234523452345243524352345234523452345234 ',hola);
-    res.send(hola);
+    attIds = req.body;
+    console.log('5234523452345243524352345234523452345234 ', attIds);
+    res.send(attIds);
 });
