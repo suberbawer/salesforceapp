a42845ee74975d76bb1ddc10eae291a462deb35b
santi <santiago@oktana.io>
2015-12-04 16:17:25 -0300

    s

--- models/database.js
+++ models/database.js
@@ -74,7 +74,7 @@ module.exports = {
     // }
     testConnection: function(req, res) {
         var pg = require('pg');
-        console.log(process.env.DATABASE_URL);
+        console.log('URL*********',process.env.DATABASE_URL);
         pg.connect(process.env.DATABASE_URL, function(err, client) {
           if (err) throw err;
           console.log('Connected to postgres! Getting schemas...');
