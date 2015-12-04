b49911d681e113838f920f12a7e7f3d28afe68f3
santi <santiago@oktana.io>
2015-12-04 16:21:47 -0300

    s

--- models/database.js
+++ models/database.js
@@ -1,86 +1,77 @@
 module.exports = {
-    // getRecords: function(req, res) {
-    //     var pg = require('pg');
-    //     //You can run command "heroku config" to see what is Database URL from Heroku belt
-    //     var conString = process.env.DATABASE_URL || "postgres://postgres:Welcome123@localhost:5432/postgres";
-    //     var login_data = new pg.LogginData(conString);
-    //     login_data.connect();
-    //     var query = login_data.query("select id, accessToken, refreshToken, instanceUrl from login_data");
-    //     query.on("row", function (row, result) {
-    //         result.addRow(row);
-    //     });
-    //     query.on("end", function (result) {
-    //         login_data.end();
-    //         res.writeHead(200, {'Content-Type': 'text/plain'});
-    //         res.write(JSON.stringify(result.rows, null, "    ") + "\n");
-    //         res.end();
-    //     });
-    // },
-    // addRecord : function(req, res){
-    //     var pg = require('pg');
-    //     var conString = process.env.DATABASE_URL ||  "postgres://postgres:Welcome123@localhost:5432/postgres";
-    //     var login_data = new pg.LogginData(conString);
-    //     login_data.connect();
-    //     var query = login_data.query("insert into login_data (accessToken, refreshToken, instanceUrl) "+
-    //                             "values ('"+req.query.aT+"','"+req.query.rT+"','"+
-    //                                 req.query.iUrl+"')");
-    //     query.on("end", function (result) {
-    //         login_data.end();
-    //         res.write('Success');
-    //         res.end();
-    //     });
-    // },
-    //  delRecord : function(req, res){
-    //     var pg = require('pg');
-    //     var conString = process.env.DATABASE_URL ||  "postgres://postgres:Welcome123@localhost:5432/postgres";
-    //     var login_data = new pg.LogginData(conString);
-    //     login_data.connect();
-    //     var query = login_data.query( "Delete from login_data Where id ="+req.query.id);
-    //     query.on("end", function (result) {
-    //         login_data.end();
-    //         res.write('Success');
-    //         res.end();
-    //     });
-    // },
-    // createTable : function(req, res){
-    //     var pg = require('pg');
-    //     var conString = process.env.DATABASE_URL || "postgres://postgres:Welcome123@localhost:5432/postgres";
-    //     var loggin_data = new pg.LogginData(conString);
-    //     loggin_data.connect();
-    //     var query = loggin_data.query( "CREATE TABLE loggin_data"+
-    //                                 "("+
-    //                                   "accesToken character varying(50),"+
-    //                                   "refreshToken character varying(50),"+
-    //                                   "instanceUrl character varying(50),"+
-    //                                   "id serial NOT NULL"+
-    //                                 ")");
-    //     query.on("end", function (result) {
-    //         loggin_data.end();
-    //         res.write('Table Schema Created');
-    //         res.end();
-    //     });
-    // },
-    // dropTable : function(req, res){
-    //     var pg = require('pg');
-    //     var conString = process.env.DATABASE_URL || "postgres://postgres:Welcome123@localhost:5432/postgres";
-    //     var login_data = new pg.LogginData(conString);
-    //     login_data.connect();
-    //     var query = login_data.query( "Drop TABLE login_data");
-    //     query.on("end", function (result) {
-    //         login_data.end();
-    //         res.write('Table Schema Deleted');
-    //         res.end();
-    //     });
-    // }
-    testConnection: function(req, res) {
+    getRecords: function(req, res) {
         var pg = require('pg');
-        console.log('URL*********',process.env.DATABASE_URL);
-        pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-            client.query('SELECT * FROM your_table', function(err, result) {
-              done();
-              if(err) return console.error(err);
-              console.log(result.rows);
-            });
+        console.log('logiin url database ', process.env.DATABASE_URL)
+        //You can run command "heroku config" to see what is Database URL from Heroku belt
+        var conString = process.env.DATABASE_URL || "postgres://postgres:Welcome123@localhost:5432/postgres";
+        var login_data = new pg.LogginData(conString);
+        login_data.connect();
+        var query = login_data.query("select id, accessToken, refreshToken, instanceUrl from login_data");
+        query.on("row", function (row, result) {
+            result.addRow(row);
+        });
+        query.on("end", function (result) {
+            login_data.end();
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            res.write(JSON.stringify(result.rows, null, "    ") + "\n");
+            res.end();
+        });
+    },
+    addRecord : function(req, res){
+        var pg = require('pg');
+        var conString = process.env.DATABASE_URL ||  "postgres://postgres:Welcome123@localhost:5432/postgres";
+        var login_data = new pg.LogginData(conString);
+        login_data.connect();
+        var query = login_data.query("insert into login_data (accessToken, refreshToken, instanceUrl) "+
+                                "values ('"+req.query.aT+"','"+req.query.rT+"','"+
+                                    req.query.iUrl+"')");
+        query.on("end", function (result) {
+            login_data.end();
+            res.write('Success');
+            res.end();
+        });
+    },
+     delRecord : function(req, res){
+        var pg = require('pg');
+        var conString = process.env.DATABASE_URL ||  "postgres://postgres:Welcome123@localhost:5432/postgres";
+        var login_data = new pg.LogginData(conString);
+        login_data.connect();
+        var query = login_data.query( "Delete from login_data Where id ="+req.query.id);
+        query.on("end", function (result) {
+            login_data.end();
+            res.write('Success');
+            res.end();
+        });
+    },
+    createTable : function(req, res){
+        console.log('logiin url database ', process.env.DATABASE_URL)
+        var pg = require('pg');
+        var conString = 'postgres://kobwxuzwrdnfbw:c8BBmA8e6B8euXT02JEmMvVTft@ec2-54-197-247-170.compute-1.amazonaws.com:5432/d3cdt1vo5k63j8';
+        var loggin_data = new pg.LogginData(conString);
+        loggin_data.connect();
+        var query = loggin_data.query( "CREATE TABLE loggin_data"+
+                                    "("+
+                                      "accesToken character varying(50),"+
+                                      "refreshToken character varying(50),"+
+                                      "instanceUrl character varying(50),"+
+                                      "id serial NOT NULL"+
+                                    ")");
+        query.on("end", function (result) {
+            loggin_data.end();
+            res.write('Table Schema Created');
+            res.end();
+        });
+    },
+    dropTable : function(req, res){
+        var pg = require('pg');
+        var conString = process.env.DATABASE_URL || "postgres://postgres:Welcome123@localhost:5432/postgres";
+        var login_data = new pg.LogginData(conString);
+        login_data.connect();
+        var query = login_data.query( "Drop TABLE login_data");
+        query.on("end", function (result) {
+            login_data.end();
+            res.write('Table Schema Deleted');
+            res.end();
         });
     }
 };
