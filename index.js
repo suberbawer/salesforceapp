45c119f0b70b021b644afcfe6611c54b731eb9b4
santi <santiago@oktana.io>
2015-12-04 13:28:43 -0300

    s

--- index.js
+++ index.js
@@ -79,9 +79,9 @@ app.get('/callback', function(req, res) {
 
 app.get('/attachments', function(req, res) {
     // if auth has not been set, redirect to index
-    if (!req.session.accessToken || !req.session.instanceUrl) { res.redirect('/'); }
+    if (!accessToken || !instanceUrl) { res.redirect('/'); }
 
-    var query = 'SELECT id, name FROM Account LIMIT 50000';
+    var query = "SELECT Id, Name FROM Account";
     // open connection with client's stored OAuth details
     var conn = new jsforce.Connection({
         accessToken: accesToken,
