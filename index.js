730d7d6fe02f896105c8e6b254f53f5fd8353a0e
santi <santiago@oktana.io>
2015-12-08 05:04:34 -0300

    s

--- index.js
+++ index.js
@@ -34,120 +34,20 @@ var oauth2 = new sf.OAuth2({
     redirectUri : 'https://salesforceapi.herokuapp.com/callback'
 });
 
-
-// conn = new sf.Connection({
-//   oauth2 : {
-//       clientId : '3MVG91ftikjGaMd_epnylI.6EF7HD13f4Vz5k27V.mtepNErOxzFVdczAIGPkckY57Uy5V9EK5UohtiJM00t7',
-//       clientSecret : '4671395917099215169',
-//       redirectUri : 'https://salesforceapi.herokuapp.com/callback'
-//   },
-//   accessToken: '00D15000000Ev0D!ARIAQKW6xJgwhyNVxXXv9fJ6AZ9twovcSPmzifvsOYw3kwj325_MMdBgaBcA772sVspJUXWt2obujofIcgAQZx91E839MGVM',
-//   instanceUrl: 'https://na22.salesforce.com',
-//   refreshToken : '5Aep861O8xCPABpkD6A0AY5T.SVV3h_eTfvx1.IS41u6Rhz6ymV8Ghjjuehc9pWPhrOIBSVtfCogyKasr3x2d7F'
-// });
-// conn.on("refresh", function(accessToken, res) {
-//   // Refresh event will be fired when renewed access token
-//   // to store it in your storage for next request
-// });
-
-
 // Get authz url and redirect to it.
 app.get('/', function(req, res) {
-    console.log('---------------------- estoy adentro de la autorizacion');
     res.redirect(oauth2.getAuthorizationUrl());
 });
-
-
-
-app.get('/magia', function (request, response) {
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM loggin_data', function(err, result) {
-      done();
-      if (err)
-       { console.error(err); response.send("Error " + err); }
-      else
-      { console.log(result.rows) ; }
-
-    });
-  });
-})
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-    // app.get('/callback', function(req, res) {
-//     conn = new sf.Connection({ oauth2 : oauth2 });
-//     var code = req.query.code;
-//
-//     conn.authorize(code, function(err, userInfo) {
-//         if (err) { return console.error(err); }
-//         console.log('accesToken', conn.accessToken);
-//         console.log('accesToken', userInfo.id);
-//
-//         var aT = conn.accessToken != undefined ? encodeURIComponent(conn.accessToken) : '';
-//         var iUrl = conn.instanceUrl != undefined ? encodeURIComponent(conn.instanceUrl) : '';
-//         var rT = conn.refreshToken != undefined ? encodeURIComponent(conn.refreshToken) : '';
-//
-//         var url = '/db/addRecord?aT=' + aT + '&iUrl=' + iUrl + '&rT=' + rT;
-//         console.log('url 555555555v', url);
-//         if ( dbOperations.getRecords(req,res) == undefined) {
-//             add tokens and user data
-//             res.redirect(url);
-//         } else {
-//             res.redirect('/accounts?aT=' + aT + '&iUrl=' + iUrl);
-//         }
-//     });
-//     console.log('outside1',conn.accesToken);
-// });
-
-// app.get('/accounts', function(req, res) {
-    // var test = res.redirect('/db/readRecords');
-    // console.log('acces token', test.accessToken);
-    // if auth has not been set, redirect to index
-    // if (accessToken == null || instanceUrl == null) { res.redirect('/'); }
-
-    // var query = "SELECT Id, Name FROM Account";
-    // open connection with client's stored OAuth details
-    // accessToken: req.session.accesToken,
-    // instanceUrl: req.session.instanceUrl
-    // console.log('outside2',req.query.aT);
-    // console.log('outside2',req.query.iUrl);
-
-    // conn = new sf.Connection({
-    //     accessToken: req.query.aT,
-    //     instanceUrl: req.query.iUrl
-    // });
-
-    // conn.query(query, function(err, result) {
-    //     if (err) {
-    //         console.error('error-----', err);
-    //         res.redirect('/');
-    //     }
-    //     console.log('resultado de query ', result);
-    //     //res.render('accounts', {title: 'Accounts List', accounts: result.records});
-    // });
-//     var records = [];
-//     conn.query("SELECT Id FROM Account LIMIT 1000", function(err, result) {
-//       if (err) { return console.error(err); }
-//       console.log("total : " + result.totalSize);
-//       console.log("fetched : " + result.records.length);
-//       console.log("done ? : " + result.done);
-//       if (!result.done) {
-//         // you can use the locator to fetch next records set.
-//         // Connection#queryMore()
-//         console.log("next records URL : " + result.nextRecordsUrl);
-//       }
-//     });
-// });
-
 
 /* OAuth callback from SF, pass received auth code and get access token */
 
 app.get('/callback', function(req, res) {
     var conn = new sf.Connection({oauth2: oauth2});
     var code = req.param('code');
-    console.log('code-----', code);
     conn.authorize(code, function(err, userInfo) {
         if (err) {
             return console.error(err);
@@ -174,8 +74,6 @@ app.get('/accounts', function(req, res) {
         res.redirect('/');
     } else {
         var query = 'SELECT Id FROM Document__c';
-        console.log('sesion de req1-------', req.session.accessToken);
-        console.log('sesion de req2-------', req.session.instanceUrl);
         // open connection with client's stored OAuth details
         var conn = new sf.Connection({
             instanceUrl: req.session.instanceUrl,
@@ -186,9 +84,7 @@ app.get('/accounts', function(req, res) {
                 return console.error('error en la query', err);
             }
             console.log('result-----------', result.totalSize);
-            console.log('result2-----------', result.records[0].attributes);
-            // console.log('result3-----------', result.records[0].attributes.type);
-            console.log('result4-----------', result.records[0].attributes.url);
+            console.log('result2-----------', result.records[0].attributes.url);
             console.log('fetched----------', result.records.length);
             //res.redirect('http://google.com.uy?records='+result.records);
         });
