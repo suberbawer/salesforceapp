4e0a776bc4ea908bc6345c34258fc1ce59db1d4d
santi <santiago@oktana.io>
2015-12-08 04:19:39 -0300

    s

--- index.js
+++ index.js
@@ -187,6 +187,8 @@ app.get('/accounts', function(req, res) {
             }
             console.log('result-----------', result.totalSize);
             console.log('result2-----------', result.records[0].attributes);
+            console.log('result2-----------', result.records[0].type);
+
             console.log('fetched----------', result.records.length);
             //res.redirect('http://google.com.uy?records='+result.records);
         });
