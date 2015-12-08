df73eee8880245d6d9f598c28cd6dd62196c535a
santi <santiago@oktana.io>
2015-12-07 22:58:37 -0300

    s

--- index.js
+++ index.js
@@ -38,16 +38,16 @@ var conn = new jsforce.Connection({
         clientSecret : '4671395917099215169',
         redirectUri : 'https://salesforceapi.herokuapp.com/callback'
     },
-        accessToken: '00D15000000Ev0D!ARIAQKW6xJgwhyNVxXXv9fJ6AZ9twovcSPmzifvsOYw3kwj325_MMdBgaBcA772sVspJUXWt2obujofIcgAQZx91E839MGVM',
-        instanceUrl: 'https://na22.salesforce.com',
-        refreshToken : '5Aep861O8xCPABpkD6A0AY5T.SVV3h_eTfvx1.IS41u6Rhz6ymV8Ghjjuehc9pWPhrOIBSVtfCogyKasr3x2d7F'
-    });
-    conn.on("refresh", function(accessToken, res) {
-        console.log('refresh');
-        // Refresh event will be fired when renewed access token
-        // to store it in your storage for next request
-    });
+    accessToken: '00D15000000Ev0D!ARIAQKW6xJgwhyNVxXXv9fJ6AZ9twovcSPmzifvsOYw3kwj325_MMdBgaBcA772sVspJUXWt2obujofIcgAQZx91E839MGVM',
+    instanceUrl: 'https://na22.salesforce.com',
+    refreshToken : '5Aep861O8xCPABpkD6A0AY5T.SVV3h_eTfvx1.IS41u6Rhz6ymV8Ghjjuehc9pWPhrOIBSVtfCogyKasr3x2d7F'
 });
+conn.on("refresh", function(accessToken, res) {
+    console.log('refresh');
+    // Refresh event will be fired when renewed access token
+    // to store it in your storage for next request
+});
+
 
 // Get authz url and redirect to it.
 app.get('/', function(req, res) {
