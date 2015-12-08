5ca4f2e5c5edd750e396d098c890df9886490622
santi <santiago@oktana.io>
2015-12-08 02:57:55 -0300

    s

--- index.js
+++ index.js
@@ -181,7 +181,7 @@ app.get('/accounts', function(req, res) {
             instanceUrl: req.session.instanceUrl,
             accessToken: req.session.accessToken
         });
-
+        console.log('connnnn0000000 ', conn);
         conn.query(query, function(err, result) {
             if (err) {
                 return console.error('error en la query', err);
