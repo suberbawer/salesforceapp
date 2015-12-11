670ec3d9df976884ae1c77c76241c2d28543f53f
santi <santiago@oktana.io>
2015-12-11 16:41:02 -0300

    s

--- index.js
+++ index.js
@@ -114,9 +114,12 @@ app.get('/getpdf', function(request, response) {
         }
     };
     var req = http.request(options, function(res) {
-        res.setEncoding('binary');
-        var binaryData = [];
+        res.setEncoding(null);
+        var binaryData = '';
         res.on('data', function (chunk) {
+            console.log('CHUNK----------  ', chunk);
+            console.log('CHUNK64----------  ', chunk.toString('base64'));
+
             binaryData += chunk;
         });
         res.on('end', function() {
