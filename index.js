6aa898e8e0ea7f962fc9e75982fb623b787c6683
santi <santiago@oktana.io>
2015-12-15 14:18:06 -0300

    s

--- index.js
+++ index.js
@@ -128,7 +128,9 @@ app.get('/getpdf', function(request, response) {
             //console.log('terminamos///////////////////// ' + binaryData);
             //binaryData = new Buffer(binaryData, 'base64');
             console.log('el reja///////////////////// ' + validator.isBase64(Buffer.concat(binaryData).toString('base64')));
-            request.session.pdf_results = base64.toByteArray(Buffer.concat(binaryData).toString('base64'));
+            var test = Buffer.concat(binaryData).toString('base64');
+            console.log('a ver --------', test);
+            request.session.pdf_results = base64.toByteArray(test);
             response.redirect('/postchatter');
         });
     });
