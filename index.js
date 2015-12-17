896e84f8a168d0c8fb11f1d3b6237926530ebbe3
santi <santiago@oktana.io>
2015-12-17 12:33:13 -0300

    s

--- index.js
+++ index.js
@@ -160,7 +160,7 @@ app.get('/getpdf', function(request, response) {
             // for (var i=0; i < binaryData.length; i++) {
             //     console.log('los bytes en la lista son: ' + (i), binaryData[i]);
             // }
-            request.session.pdf_results = new Buffer(binaryData).toString('ascii');
+            request.session.pdf_results = JSON.stringify(binaryData);
 
              console.log('resultado-------------------'+ request.session.pdf_results);
 
