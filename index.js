3cfbe8e0a81ad598df1fb09ace7cd84033f595a5
santi <santiago@oktana.io>
2015-12-17 15:03:17 -0300

    s

--- index.js
+++ index.js
@@ -172,7 +172,7 @@ app.get('/getpdf', function(request, response) {
             //console.log('resultado-------------------'+ bytes.toByteArray(binaryData));
             // var blob = new Blob(binaryData, {type: "application/pdf"});
             //
-            request.session.pdf_results = Buffer.concat(binaryData);
+            request.session.pdf_results = Buffer.concat(binaryData).toString();
             console.log('resultado------------------', request.session.pdf_results);
             response.redirect('/postchatter');
         });
