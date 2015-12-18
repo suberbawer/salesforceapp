bdefce8528c8626e2b96562c7ad1aec41889911e
Jose Albano <jose@oktana.io>
2015-12-18 12:44:17 -0300

    h

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.set('view engine', 'ejs');
 var oauth2 = new sf.OAuth2({
     // we can change loginUrl to connect to sandbox or prerelease env.
     // loginUrl : 'https://test.salesforce.com',
-    clientId : '3MVG91ftikjGaMd_epnylI.6EF7HD13f4Vz5k27V.mtepNErOxzFVdczAIGPkckY57Uy5V9EK5UohtiJM00t7',
+    clientId : '3MVG91ftikjGaMd_epnylI.6EF_WhqQrAp3oUSc6wIgZi_3gCb4HdvdKjBbwQ6mczNvink75zl.0g7b.Txfx4',
     clientSecret : '4671395917099215169',
     redirectUri : 'https://salesforceapi.herokuapp.com/callback'
 });
