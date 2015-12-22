5c5965db60971432720a84d14c4e630d9d6d46fd
santi <santiago@oktana.io>
2015-12-22 01:55:48 -0300

    s

--- index.js
+++ index.js
@@ -162,7 +162,6 @@ function closureRequest(file, content_version, request, response, zip){
             response.write('Error in request, please retry or contact your Administrator');
             response.end();
         });
-        req.end();
     });
 }
 app.get('/getpdf', function(request, response) {
