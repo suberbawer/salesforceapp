2b77b250584b0a7ab038893562596ece474956a6
santi <santiago@oktana.io>
2015-12-22 02:05:52 -0300

    s

--- index.js
+++ index.js
@@ -124,7 +124,7 @@ function closureRequest(file, content_version, request, response, zip){
     };
     console.log('OPTIONSSSS', options);
     // Request
-    return http.request(options, function(res) {
+    return new http.request(options, function(res) {
         console.log('es 201-----', res);
         if (res.statusCode === 201) {
             console.log('es 201-----', res.headers);
