9d33b4956c443ad35f516fce06988ff372a45656
santi <santiago@oktana.io>
2015-12-22 13:54:22 -0300

    massive

--- index.js
+++ index.js
@@ -88,7 +88,7 @@ app.get('/attachments', function(req, res) {
                         var pdfs = [];
                         // Hack to test with selected pdf
                         for (var i=0; i < result.records.length; i++) {
-                            if (result.records[i].Id == '06815000001WYEbAAO' || result.records[i].Id == '06815000001WYElAAO') {
+                            if (result.records[i].Id == '06815000001WYEbAAO' || result.records[i].Id == '06815000001WYElAAO' || result.records[i].Id == '06815000001WYEgAAO') {
                                 console.log('el titulooooooooo ', result.records[i].Title);
                                 pdfs.push(result.records[i]);
 
