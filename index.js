999d29920ac79c9dbe6db0f59c75bf079174ec1a
santi <santiago@oktana.io>
2015-12-22 12:37:56 -0300

    s

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ app.get('/attachments', function(req, res) {
                 instanceUrl: req.session.instanceUrl,
                 accessToken: req.session.accessToken
             });
-            //|| result.records[i].Id == '06815000001WYEgAAO' 
+            //|| result.records[i].Id == '06815000001WYEgAAO'
             // First query on documents then into content documents to retrieve the file
             conn.query(query, function(err, result) {
                 if (err) {
@@ -87,13 +87,13 @@ app.get('/attachments', function(req, res) {
                     if (result.done && result.records.length > 0) {
                         var pdfs = [];
                         // Hack to test with selected pdf
-                        for (var i=0; i < result.records.length; i++) {
-                            if (result.records[i].Id == '06815000001WYEbAAO' || result.records[i].Id == '06815000001WYElAAO') {
-                                console.log('el titulooooooooo ', result.records[i].Title);
-                                pdfs.push(result.records[i]);
-
-                            }
-                        }
+                        // for (var i=0; i < result.records.length; i++) {
+                            // if (result.records[i].Id == '06815000001WYEbAAO' || result.records[i].Id == '06815000001WYElAAO') {
+                            //     console.log('el titulooooooooo ', result.records[i].Title);
+                            //     pdfs.push(result.records[i]);
+                            //
+                            // }
+                        // }
 
                         if (pdfs.length == 0) {
                             pdfs = result.records;
@@ -142,6 +142,7 @@ app.get('/getpdf', function(request, response) {
                 // Write file with chunks
                 file.write(chunk);
             });
+
             res.on('end', function() {
                 file.end();
                 count++;
