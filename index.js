48e0ee7100198f458b2a4c61faa1b9c3ba8b0502
santi <santiago@oktana.io>
2015-12-28 13:33:59 -0300

    s

--- index.js
+++ index.js
@@ -172,13 +172,13 @@ function getDocuments(request, response, accessToken) {
         };
         for (var i=0; i < files.length; i++) {
 
-            zip.append(fs.createReadStream(files[i].Title), {name: files[i].Title});
+            zip.file(fs.createReadStream(files[i].Title), {name: files[i].Title});
             // When finish close zip and post into chatter
         }
         // if (!err && i+1 == files.length) {
-            zip.finalize(function(err, bytes) {
+            zip.finalize(function(err) {
                 if (!err) {
-                    console.log('GET DOCUMENTS ASYNC AND ZIPIT REDIRECT TO POST', bytes);
+                    console.log('GET DOCUMENTS ASYNC AND ZIPIT REDIRECT TO POST', err);
                     postToChatter(request, response, accessToken);
                 }
             });
