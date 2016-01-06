cd4a6d373afa71219d1d6e913f3930ebe37f06fc
santi <santiago@oktana.io>
2016-01-06 13:32:35 -0300

    s

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.get('/', function(req, res) {
 });
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+    console.log('Node app is running on port', app.get('port'));
 });
 
 /* OAuth callback from SF, pass received auth code and get access token */
