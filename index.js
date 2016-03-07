faffd8eb5141655d50266ffe1bf2c5eb7754ec73
santi <santiago@oktana.io>
2016-03-07 18:16:43 -0300

    s

--- index.js
+++ index.js
@@ -84,7 +84,7 @@ app.get('/', function(req, res) {
         clientSecret : '5644212675256863801',
         redirectUri : 'https://salesforceapi.herokuapp.com/callback'
     });
-    res.redirect(oauth2.getAuthorizationUrl({ scope: 'api id web refresh_token' }));
+    res.redirect(oauth2.getAuthorizationUrl({ scope: 'api id web' }));
 });
 
 app.listen(app.get('port'), function() {
