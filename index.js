772492f10cfc081de8731a3b4275ff40cfdbd06a
santi <santiago@oktana.io>
2016-04-04 18:16:37 -0300

    s

--- index.js
+++ index.js
@@ -206,17 +206,17 @@ app.get('/download-zip-file', function(req, res) {
 //function donwloadZipFile(res, req) {
     var zip         = archiver.create('zip', {});
     var output      = fs.createWriteStream('outputZip.zip');
-        // Bind zip to output
+    // Bind zip to output
     zip.pipe(output);
 
-    // archive.on('error', function(err) {
-    //     res.status(500).send({error: err.message});
-    // });
-    // //on stream closed we can end the request
-    // res.on('close', function() {
-    //     console.log('Archive wrote %d bytes', archive.pointer());
-    //     return res.status(200).send('OK').end();
-    // });
+    archive.on('error', function(err) {
+        res.status(500).send({error: err.message});
+    });
+    //on stream closed we can end the request
+    res.on('close', function() {
+        console.log('Archive wrote %d bytes', archive.pointer());
+        return res.status(200).send('OK').end();
+    });
     //this is the streaming magic
     zip.append(fs.createReadStream('file.txt'), {name:'file.txt'});
     //you can add a directory using directory function
