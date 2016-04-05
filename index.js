0a936c2682a00be166bee4d33a3890de7052ea56
santi <santiago@oktana.io>
2016-04-05 14:37:34 -0300

    s

--- index.js
+++ index.js
@@ -262,7 +262,7 @@ function postToChatter(request, response, credentials) {
         res.on('end', function() {
             var parsedBody = JSON.parse(body);
             //console.log('a ver la response ahora-- ', parsedBody);
-            console.log('a ver la response ahora22222-- ', parsedBody.actor.content + ' - ' + parsedBody['actor'].content);
+            console.log('a ver la response ahora22222-- ', JSON.stringify(parsedBody.actor) + ' - ' + JSON.stringify(parsedBody['actor']));
             // console.log('AHORA SI--', body);
             // console.log('version id', JSON.stringify(body));
             response.sendStatus(res.statusCode);
