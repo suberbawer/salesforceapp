1ae70b539b0274276c168d90d6b246a0ed34ba12
santi <santiago@oktana.io>
2016-04-05 17:57:37 -0300

    s

--- index.js
+++ index.js
@@ -264,7 +264,7 @@ function postToChatter(request, response, credentials) {
             console.log('a ver la response ahora22222-- ', parsedBody.capabilities.content.versionId);
             //response.sendStatus(res.statusCode);
             //response.send('Id', parsedBody.capabilities.content.versionId);
-            response.writeHead(res.statusCode, { 'Content-Type': 'application/json' });
+            console.log('RESPONSE ', response);
             response.json({versionId: parsedBody.capabilities.content.versionId});
             response.end();
         });
